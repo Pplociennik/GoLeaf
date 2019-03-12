8df2e569bae07daec087c90adf33bb0263adb198
Pplociennik <przemx.84@gmail.com>
2019-03-12 15:03:13 +0100

    Added server core (starter)

---
+++ Server/src/main/java/com/goaleaf/engine/GoaLeaf/GoaLeafApplication.java
@@ -0,0 +1,13 @@
+package com.goaleaf.engine.GoaLeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoaLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoaLeafApplication.class, args);
+	}
+
+}
