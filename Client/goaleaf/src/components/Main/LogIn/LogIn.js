50f59a16c6804335e44d207a3d35478c739d5493
Marek Pikosz <marek.pikosz@gmail.com>
2019-03-18 19:07:31 +0100

    login and signin pages styling

--- Client/goaleaf/src/components/Main/LogIn/LogIn.js
+++ Client/goaleaf/src/components/Main/LogIn/LogIn.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
 import styles from './LogIn.module.scss'
+import { Link } from 'react-router-dom'
 
 class LogIn extends Component {
+
+  state = {
+    login: null,
+    password: null
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state)
+  }
   render() {
     return (
       <div className={styles.LogIn}>
-        </div>
-      )
+      <form onSubmit={ this.handleSubmit }>
+        <h1> Log In </h1>
+        <label>
+          login 
+          <input className={styles.InputField} type="text" id="login" onChange={ this.handleChange } />
+        </label>
+        <label>
+          password 
+          <input className={styles.InputField} type="password" id="password" onChange={ this.handleChange } />
+        </label>
+          <div className={styles.Buttons}>
+            <input type="submit" value="log in" />
+            <Link to='/signin'><input type="button" value="sign in" /></Link>
+          </div>
+      </form>
+      </div>
+    )
+  }
 }
 
 export default LogIn;
--- Client/goaleaf/src/components/Main/SignIn/SignIn.js
+++ Client/goaleaf/src/components/Main/SignIn/SignIn.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react'
 import styles from './SignIn.module.scss'
+import { Link } from 'react-router-dom'
 
 class SignIn extends Component {
+
+  state = {
+    email: null,
+    login: null,
+    password: null
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state)
+  }
   render() {
     return (
       <div className={styles.SignIn}>
-        Sign In
+      <form onSubmit={ this.handleSubmit }>
+        <h1> Sign In </h1>
+        <label>
+          email 
+          <input className={styles.InputField} type="email" id="email" onChange={ this.handleChange } />
+        </label>
+        <label>
+          login 
+          <input className={styles.InputField} type="text" id="login" onChange={ this.handleChange } />
+        </label>
+        <label>
+          password 
+          <input className={styles.InputField} type="password" id="password" onChange={ this.handleChange } />
+        </label>
+          <div className={styles.Buttons}>
+            <input type="submit" value="sign in" />
+            <Link to='/login'><input type="button" value="log in" /></Link>
+          </div>
+      </form>
       </div>
     )
   }
