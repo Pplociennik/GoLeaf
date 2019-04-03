242a2c06edacc1b55fe8fd5014ae667fe2f2c3c9
Pplociennik <przemx.84@gmail.com>
2019-04-04 00:01:17 +0200

    Update token validation

--- Server/src/main/java/com/goaleaf/services/JwtService.java
+++ Server/src/main/java/com/goaleaf/services/JwtService.java
@@ -3,12 +3,16 @@ package com.goaleaf.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.impl.JWTParser;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.Payload;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 import javax.xml.bind.DatatypeConverter;
+
+import java.util.Date;
 
 import static com.goaleaf.security.SecurityConstants.SECRET;
 
@@ -22,6 +26,14 @@ public class JwtService {
         JWTVerifier verifier = JWT.require(algorithm)
                 .build(); //Reusable verifier instance
         DecodedJWT jwt = verifier.verify(token);
+
+        JWTParser parser = new JWTParser();
+        Payload jwtInfo = parser.parsePayload(jwt.getPayload());
+        Date expDate = jwtInfo.getExpiresAt();
+
+        if (!new Date().before(expDate))
+            return false;
+
         return true;
     }
 }
