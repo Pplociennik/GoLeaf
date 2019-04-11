3af21bfb25e29b807a3383a62aaf8dc8e69b2a32
pifls <marek.pikosz@gmail.com>
2019-04-11 10:25:21 +0200

    delete token from localstorage in INVALID_USER action

--- Client/goaleaf/src/components/routes/NewHabit/NewHabit.js
+++ Client/goaleaf/src/components/routes/NewHabit/NewHabit.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './NewHabit.scss'
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 class NewHabit extends Component {
 
@@ -10,11 +11,24 @@ class NewHabit extends Component {
     category: 'NONE',
     invite: [],
     private: false,
-    recurrence: 'daily'
+    recurrence: 'daily',
+    errorMsg: ''
   }
+
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state)
+    console.log(this.state);
+    axios.post('/api/habits/new-habit', {
+
+        // TODO //
+
+    })
+    .then(res => {
+                  this.props.history.push('/');
+                  window.location.reload();
+                 }
+    ).catch(err => this.setState({errorMsg: err.response.data.message}))
+
   }
   handleChange = e => {
     this.setState({[e.target.id]: e.target.value})
@@ -30,19 +44,24 @@ class NewHabit extends Component {
   }
 
   render() {
+    let errorMsg = <div className="ErrorMsg">{ this.state.errorMsg }</div>
     
     return (
       <div className="NewHabit">
         <form onSubmit={ this.handleSubmit } autoComplete="off" >
           <h1>New Habit</h1>
-          <input className="TitleInput" type="text" id="title" placeholder="what will be your next challenge?" onChange={ this.handleChange } autoFocus />
+          <input className="TitleInput" type="text" id="title" placeholder="what will be your next challenge?" onChange={ this.handleChange } maxLength="40" autoFocus />
           <h2>Be sure to add a category..</h2>
           <div className="Categories">
             <input className={this.state.category === 'NONE' ? 'category noneChosen none' : ' category none'} type="button" value="NONE" onClick={ this.handleChangeCategory } />
-            <input className={this.state.category === 'HEALTH' ? 'category healthChosen health' : 'category health'} type="button" value="HEALTH" onClick={ this.handleChangeCategory }/>
             <input className={this.state.category === 'DIET' ? 'category dietChosen diet' : 'category diet'} type="button" value="DIET" onClick={ this.handleChangeCategory }/>
+            <input className={this.state.category === 'SPORT' ? 'category sportChosen sport' : 'category sport'} type="button" value="SPORT" onClick={ this.handleChangeCategory }/>
+            <input className={this.state.category === 'HEALTH' ? 'category healthChosen health' : 'category health'} type="button" value="HEALTH" onClick={ this.handleChangeCategory }/>
+            <input className={this.state.category === 'SOCIAL' ? 'category socialChosen social' : 'category social'} type="button" value="SOCIAL" onClick={ this.handleChangeCategory }/>
+            <input className={this.state.category === 'FAMILY' ? 'category familyChosen family' : 'category family'} type="button" value="FAMILY" onClick={ this.handleChangeCategory }/>
             <input className={this.state.category === 'STUDY' ? 'category studyChosen study' : 'category study'} type="button" value="STUDY" onClick={ this.handleChangeCategory }/>
             <input className={this.state.category === 'WORK' ? 'category workChosen work' : 'category work'} type="button" value="WORK" onClick={ this.handleChangeCategory }/>
+            <input className={this.state.category === 'MONEY' ? 'category moneyChosen money' : 'category money'} type="button" value="MONEY" onClick={ this.handleChangeCategory }/>
           </div>
           <h2>Want to stay incognito? Set habit as private</h2>
           <div className="Privacy">
@@ -50,6 +69,7 @@ class NewHabit extends Component {
           <input className={this.state.private === true ? 'privacyChosen' : ''} type="button" value="private" onClick={ this.handleChangePrivacy } />
           </div>
           <input className="submit" type="submit" value="Create habit" />
+          { errorMsg }
         </form>
       </div>
     )
@@ -58,7 +78,7 @@ class NewHabit extends Component {
 
 const mapStateToProps = state => {
   return {
-    users: state.users
+    userLogged: state.userLogged
   }
 }
 
